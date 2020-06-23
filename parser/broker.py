09b74bcf699712081e0b5d3a41f7b81dc179d505
Douglas <douglasnomizo@outlook.com>
2020-06-23 20:00:57 -0300

    [infra, parser] replace SNS with SQS and ElasticMQ for local dev

--- parser/broker.py
+++ parser/broker.py
@@ -2,13 +2,18 @@ import os
 import json
 import boto3
 
-sns = boto3.client('sns')
-topic = sns.create_topic(Name=os.getenv('SNS_TOPIC_NAME'))
+sqs = boto3.client('sqs',
+                   endpoint_url=os.getenv('SQS_ENDPOINT_URL'),
+                   region_name=os.getenv('AWS_REGION'),
+                   use_ssl=False)
+response = sqs.create_queue(QueueName=os.getenv('SQS_QUEUE_NAME'))
 
 
 def publish(message):
-    sns.publish(
-        TopicArn=topic['TopicArn'],
-        Message=json.dumps({'default': json.dumps(message)}),
-        MessageStructure='json'
+    print('Sending new report')
+    sqs.send_message(
+        QueueUrl=response['QueueUrl'],
+        MessageBody=json.dumps(message),
+        MessageAttributes={'event_type': {
+            'DataType': 'String', 'StringValue': 'report_parsed_created'}},
     )
